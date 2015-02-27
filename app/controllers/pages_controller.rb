b962ea395622ab1cc279f6a004549fe02ed313e8
phoodily <phillip@foodily.com>
2015-02-27 00:47:05 -0800

    heroku fix for match in routes #3

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,11 +2,7 @@ class PagesController < ApplicationController
 
 def show
   # filter the params[:id] here to allow only certain values like
-  #if params[:id].match /browse|about/
-  #  render :partial => params[:id]
-  #else
-  #  render :file => "/path/to/some/404_template", :status => 404
-  #end
+  
 end
 
 end
